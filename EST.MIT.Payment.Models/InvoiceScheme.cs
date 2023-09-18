e78577d35326ae94111e5845ae9bc52d58e358a2
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-09-18 08:21:35 +0100

    Codebase changes

--- EST.MIT.Payment.Models/InvoiceScheme.cs
+++ EST.MIT.Payment.Models/InvoiceScheme.cs
@@ -7,5 +7,5 @@ public class InvoiceScheme
     [JsonProperty("schemeType")]
     public string SchemeType { get; init; } = default!;
     [JsonProperty("invoices")]
-    public List<Invoice> Invoices { get; init; } = default!;
+    public List<PaymentRequestsBatch> Invoices { get; init; } = default!;
 }
---
+++ EST.MIT.Payment.Models/PaymentRequestsBatch.cs
@@ -0,0 +1,31 @@
+using Newtonsoft.Json;
+
+namespace EST.MIT.Payment.Models;
+
+public class PaymentRequestsBatch
+{
+    [JsonProperty("id")]
+    public string Id { get; init; } = default!; 
+    [JsonProperty("accountType")]
+    public string AccountType { get; init; } = default!;
+    [JsonProperty("organisation")]
+    public string Organisation { get; init; } = default!;
+    [JsonProperty("schemeType")]
+    public string SchemeType { get; init; } = default!;
+    [JsonProperty("paymentType")]
+    public string PaymentType { get; init; } = default!;    
+    [JsonProperty("paymentRequests")]
+    public List<PaymentRequest> PaymentRequests { get; init; } = default!;
+    [JsonProperty("status")]
+    public string Status { get; init; } = default!;
+    [JsonProperty("reference")]
+    public string Reference { get; set; } = default!;
+    [JsonProperty("created")]
+    public DateTime Created { get; init; } = DateTime.UtcNow;
+    [JsonProperty("updated")]
+    public DateTime? Updated { get; init; }
+    [JsonProperty("createdBy")]
+    public string CreatedBy { get; init; } = default!;
+    [JsonProperty("updatedBy")]
+    public string UpdatedBy { get; init; } = default!;
+}
